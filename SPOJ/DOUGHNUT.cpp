59a0c47af9e1dace93a6dd72b30b4ffecb1fc118
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2015-04-03 02:21:54 +0530

    Create DOUGHNUT.cpp

---
+++ SPOJ/DOUGHNUT.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int c,k,w;
+		cin>>c>>k>>w;
+		if(c*w<=k)
+			cout<<"yes"<<endl;
+		else
+			cout<<"no"<<endl;
+	}
+	
+	return 0;
+}
