e83924543412ec755d84a12456058bf46c284dca
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2015-04-04 03:30:46 +0530

    finds highest prime factors of a given number in an efficient manner

---
+++ PrimeFactors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++>
+
+using namespace std;
+void primeFactors(long n)
+{
+  long max=0;
+  while (n%2 == 0)
+    {
+      max=2;
+      n = n/2;
+    }
+    long m = sqrt(n);
+    for (int i = 3; i <= m; i = i+2){
+        while (n%i == 0){
+            cout<<i;
+            n = n/i;
+        }
+    }
+    if (n > 2)
+      cout<<n;
+}
+
+int main()
+{
+  long t,n;
+  cin>>t;
+  while(t--){
+    cin>>n;
+    primeFactors(n);
+  }
+
+  return 0;
+}
