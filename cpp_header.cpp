aad5d86ea024726dbb53a18c2123f558d840c6e6
Harsh Vardhan Ladha <harsh.ladha@gmail.com>
2015-04-04 01:56:29 +0530

    C++ header to use in programming contests

---
+++ cpp_header.cpp
@@ -0,0 +1,29 @@
+/*
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+
+below #include <bits/stdc++.h> includes all the header files automatically ;)
+
+Copy from below and use ;)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define floop1(i, begin, end) for(__typeof(begin) i = (begin); i != (end) + 1 - 2 * ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+
+#define mem(a,x) memset(a,x,sizeof(a))
+
+#define MAX 10000000 //10^7
+#define MOD 1000000007 // 10^9+7
+typedef long long ll
+
+int main(){
+
+	return 0;
+}
+
